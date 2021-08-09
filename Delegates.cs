bbe209b521a7adbbfcb071009ae01a3e434697f8
ChickenBubbles <86927102+chickenbubbles@users.noreply.github.com>
2021-08-09 16:50:51 -0300

    Add files via upload

---
+++ Delegates.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace Delegates
+
+{
+    class Program
+    {
+        public delegate void MathLib(float x, float y);
+        static MathLib mathLib;
+
+        static void Main(string[] args)
+        {
+            SetMethods();
+            mathLib(10f, 10f);
+        }
+
+        static void SetMethods()
+        {
+            mathLib += Add;
+            mathLib += Sub;
+            mathLib += Mult;
+        }
+
+        static void Add(float x, float y)
+        {
+            Console.WriteLine(x + y);
+        }
+
+        static void Sub(float x, float y)
+        {
+            Console.WriteLine(x - y);
+        }
+
+        static void Mult(float x, float y)
+        {
+            Console.WriteLine(x * y);
+        }
+
+
+    }
+}
